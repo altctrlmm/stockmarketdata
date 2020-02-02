e4ee9d1ae2032056bc782f81d89579c3ef8f8c3e
Micah <nothx@cnm.edu>
2020-02-02 01:02:02 -0700

    Create test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 01:00:49 2020
+
+@author: micah
+"""
+
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(1) == 2
