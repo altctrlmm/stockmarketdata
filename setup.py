d96394729eaaee34692896c89a10082a742d65c0
Micah <nothx@cnm.edu>
2020-02-03 14:03:57 -0700

    Delete workfile pythonpublish

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="altctrlmm", # Replace with your own username
-    version="0.0.1",
-    author="Micah",
-    author_email="nothx@example.com",
+    name="rhutils", 
+    version="0.0.2",
+    author="altctrlmm",
+    author_email="micahmrocks@gmail.com",
     description="Robinhood Utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/rhutils",
+    url="https://github.com/altctrlmm/rhutils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
