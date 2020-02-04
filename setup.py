46980e62d0cc02d73834918a5e61ac14eefc6930
Micah <nothx@cnm.edu>
2020-02-03 20:03:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rhutils", 
-    version="0.0.9",
+    version="0.1.0",
     author="altctrlmm",
     author_email="micahmrocks@gmail.com",
     description="Robinhood Utilities",
